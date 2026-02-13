5f92d073a16d78c7f7d3a072211fc1a2e02bb74b
furioussoulk <szj13776197236@163.com>
2026-02-13 12:18:17 +0000

    fix: allow vercel sandbox host in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    allowedHosts: ["sb-77dmcwlk18j9.vercel.run"]
   }
 })
